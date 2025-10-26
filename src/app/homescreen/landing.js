db9fff3804f30781631a00184a91bdd31bc273b6
mauro-sousa <maurodesousa94@gmail.com>
2025-10-26 23:16:43 +0000

    N/A

--- src/app/homescreen/landing.js
+++ src/app/homescreen/landing.js
@@ -41,25 +41,7 @@ export default function Landing() {
                     </div>
                 </div>
 
-                <div
-                    className="absolute bottom-0 left-0 right-0 top-auto w-full h-16 overflow-hidden pointer-events-none"
-                    style={{ transform: "translateZ(0)" }}
-                >
-                    <svg
-                        className="absolute bottom-0 overflow-hidden"
-                        xmlns="http://www.w3.org/2000/svg"
-                        preserveAspectRatio="none"
-                        version="1.1"
-                        viewBox="0 0 2560 100"
-                        x="0"
-                        y="0"
-                    >
-                        <polygon
-                            className="text-gray-100 fill-current"
-                            points="2560 0 2560 100 0 100"
-                        ></polygon>
-                    </svg>
-                </div>
+
             </div>
 
             {/* Services  and 2nd section*/}
